8adaa438d9ec9c9e58081023dde916bd5f62d343
Tahreem-riaz <totehreem101@gmail.com>
2026-09-08 19:25:53 +0500

    Add the question of program no 1

--- 04_type_conversion.py
+++ 04_type_conversion.py
@@ -11,3 +11,10 @@ Topics: Type Conversion, Type Casting, Implicit and
 # PART A: BASIC TYPE CASTING
 # ========================================================
 
+# Q1. Convert the following values:
+#
+#     45       → float
+#     78.9     → int
+#     250      → string
+#
+#     Print each converted value.
