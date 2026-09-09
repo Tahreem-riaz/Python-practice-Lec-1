aa78e4a6cf0ab161fabdfd7ef1e18a1ac5f3bf45
Tahreem-riaz <totehreem101@gmail.com>
2026-09-09 07:31:59 +0500

    Add the the step 1 :Get user information

--- 05_mini_project.py
+++ 05_mini_project.py
@@ -22,3 +22,23 @@ Concepts Used:
 print("=" * 50)
 print("          PERSONAL BUDGET CALCULATOR")
 print("=" * 50)
+
+# --------------------------------------------------------
+# STEP 1: GET USER INFORMATION
+# --------------------------------------------------------
+
+# Ask the user for:
+# - Name
+# - Monthly budget
+# - Food expenses
+# - Transport expenses
+# - Entertainment expenses
+
+
+# Write your code below:
+
+name = input("Enter your name: ")
+monthly_budget = float(input("Enter your monthly budget: "))
+food_expenses = float(input("Enter your food expenses: "))
+transport_expenses = float(input("Enter your transport expenses: "))
+entertainment_expenses = float(input("Enter your entertainment expenses: "))
