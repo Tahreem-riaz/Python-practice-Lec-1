8d97b4788c1333a57c55fc0a53ae19ac2e144404
Tahreem-riaz <totehreem101@gmail.com>
2026-09-09 06:51:25 +0500

    Add the heading of file mini_project

---
+++ 05_mini_project.py
@@ -0,0 +1,16 @@
+"""
+========================================================
+          LECTURE 1 - FILE 5: MINI PROJECT
+========================================================
+Project: Personal Budget Calculator
+
+Concepts Used:
+- Variables
+- Data Types
+- Arithmetic Operators
+- Comparison Operators
+- Logical Operators
+- Type Conversion
+- User Input
+========================================================
+"""
