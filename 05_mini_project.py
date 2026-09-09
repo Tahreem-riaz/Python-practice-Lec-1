88b23ea0c56c6ce1ef6f18613334b0836d6b2a46
Tahreem-riaz <totehreem101@gmail.com>
2026-09-09 07:35:10 +0500

    Add the step 3:Calculate the remaining money

--- 05_mini_project.py
+++ 05_mini_project.py
@@ -42,3 +42,32 @@ monthly_budget = float(input("Enter your monthly budget: "))
 food_expenses = float(input("Enter your food expenses: "))
 transport_expenses = float(input("Enter your transport expenses: "))
 entertainment_expenses = float(input("Enter your entertainment expenses: "))
+
+
+# --------------------------------------------------------
+# STEP 2: CALCULATE TOTAL EXPENSES
+# --------------------------------------------------------
+
+# Calculate the total amount spent.
+
+
+# Write your code below:
+
+total_expenses = food_expenses + transport_expenses + entertainment_expenses
+
+print("Total Expenses:", total_expenses, "PKR")
+
+# --------------------------------------------------------
+# STEP 3: CALCULATE REMAINING MONEY
+# --------------------------------------------------------
+
+# Calculate how much money remains after expenses.
+
+
+# Write your code below:
+
+remaining_money = monthly_budget - total_expenses
+
+print("Remaining Money:", remaining_money, "PKR")
+
+
